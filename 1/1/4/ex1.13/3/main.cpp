bff60c4d9cda14c468badd9a0e6ec52f882ea3fb
azjf <a@z.jf>
2022-01-20 14:25:22 +0800

    1.4

---
+++ 1/1/4/ex1.13/3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int v1 = 0, v2 = 0;
+    std::cout << "Please enter two numbers:" << std::endl;
+    std::cin >> v1 >> v2;
+    if (v1 < v2) {
+        for (int i = v1; i <= v2; ++i) {
+            std::cout << i << " ";
+        }
+    } else {
+        for (int i = v1; i >= v2; --i) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
---
+++ 1/1/4/ex1.16/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int sum = 0, v = 0;
+    while (std::cin >> v) {
+        sum += v;
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
